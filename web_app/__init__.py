00d0373cc901295dba0bde20065bb00f49ec79ca
lsjttkm <ljk58@georgetown.edu>
2024-12-16 17:47:14 -0500

    deploying error fix (attempt 1)

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
 
-def create_app():
-    # Specify the `template_folder` for HTML templates
-    app = Flask(__name__, template_folder="templates")
-    
-    from web_app.routes.home_routes import home_bp
-    from web_app.routes.blackjack_routes import blackjack_bp
+from web_app.routes.home_routes import home_bp
+from web_app.routes.blackjack_routes import blackjack_bp
 
+def create_app():
+    app = Flask(__name__)
     app.register_blueprint(home_bp)
     app.register_blueprint(blackjack_bp)
     return app
 
-# Create an app instance for Flask CLI
-app = create_app()
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
